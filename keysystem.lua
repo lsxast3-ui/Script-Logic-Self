237e3a121b7436dc386ef4f3cd1d393dc71781fc
lsxast3-ui <lsxast3@gmail.com>
2026-06-16 16:40:03 +0700

    Update keysystem.lua

--- keysystem.lua
+++ keysystem.lua
@@ -9,7 +9,7 @@ local playerGui = player:WaitForChild("PlayerGui")
 local SCRIPT_URL = "https://raw.githubusercontent.com/lsxast3-ui/Script-Logic-Self/main/NexusMenu.lua"
 
 local VALID_KEYS = {
-	["NEXUS-123"] = true,
+	["123"] = true,
 	["VIP-ACCESS"] = true,
 	["ADMIN-999"] = true
 }
